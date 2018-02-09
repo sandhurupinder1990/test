2f081e6f09fcff32227cc7f7aebb98b9b56caef1
Rupinder Sandhu <rsandhu2@unl.edu>
2018-02-09 10:34:04 -0600

    filtered out annnumber=edge for fish data and created scatterplot for length vs scalelength

--- fish-analysis.R
+++ fish-analysis.R
@@ -1,8 +1,13 @@
 # .libPaths("P:/RLibrary2")
 library(tidyverse)
-
+library(ggplot2)
 fish_data <- read_csv("Gaeta_etal_CLC_data_1.csv")
 
 fish_data %>% 
   mutate(length_cat = ifelse(length>300,"big", "small")) %>% 
   filter( scalelength >= 1) ->fish_data_cat
+
+single_fish <-filter(fish_data,annnumber== "EDGE")
+  
+
+ggplot(data=single_fish, aes(x=length, y= scalelength, color=lakeid))+ geom_point() 
